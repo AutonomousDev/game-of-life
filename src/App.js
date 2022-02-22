8219793f1aa8a01188f25249940914634984c50c
Cameron Bowers <cameronbowers0@gmail.com>
2022-02-21 21:05:04 -0800

    Added a slider for game speed.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import React, { useCallback, useState, useRef } from "react";
+import React, { useCallback, useState, useRef, useEffect } from "react";
 import produce from 'immer';
 
 import Container from 'react-bootstrap/Container';
@@ -9,6 +9,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // Global config variables
 const numRows = 50;
 const numCols = 50;
+
+const gameSpeedConstant = 1000;
+//
+function Example() {
+  
+}
+
 
 // Operations are used to check every neighbor cell going clockwise
 const operations = [
@@ -22,16 +29,21 @@ const operations = [
   [-1, 1]
 ]
 
-const generateEmptyGrid = () =>{
+const generateEmptyGrid = () => {
   // Create an array with each place containing another array for rows and columns
   const rows = [];
-    for (let i = 0; i < numRows; i++) {
-      rows.push(Array.from(Array(numCols), () => 0));
-    }
-    return rows;
+  for (let i = 0; i < numRows; i++) {
+    rows.push(Array.from(Array(numCols), () => 0));
+  }
+  return rows;
 }
 
 function App() {
+  // Variable to change game speed mid game
+  const [gameSpeed, setGameSpeed] = useState(50);
+  const gameSpeedRef = useRef(gameSpeed);
+  gameSpeedRef.current = gameSpeed;
+
   const [grid, setGrid] = useState(() => {
     return generateEmptyGrid();
   });
@@ -42,7 +54,7 @@ function App() {
   // runningRef makes the current running state availbe to other functions
   const [running, setRunning] = useState(false);
   const runningRef = useRef(running);
-  runningRef.current = running
+  runningRef.current = running;
 
   // Checks the current running state before running the simulation
   const runSimulation = useCallback(() => {
@@ -61,7 +73,7 @@ function App() {
               const newK = k + y;
 
               // Checks neighbors population
-              if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) { 
+              if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
                 // Since live cells are 1 and dead cells are 0 += adds to the
                 neighbors += g[newI][newK]
               }
@@ -77,49 +89,57 @@ function App() {
         }
       })
     })
-    setTimeout(runSimulation, 100) // updates ever 100 milisecounds
-  }, []) 
+
+    // Linked to the game speed slider to dynamically change the speed mid game
+    setTimeout(runSimulation, (gameSpeedConstant/gameSpeedRef.current)) 
+  }, [])
 
   return (
     <Container>
       {/** If not running button says start, else it says stop */}
-      <div class="mt-2 mb-2">
-      <button
-        class="btn btn-success m-1"
-        onClick={() => {
-          setRunning(!running);
-          if (!running) {
-            runningRef.current = true;
-            runSimulation();
-          }
-        }}
-      >
-        {running ? 'Stop' : 'Start'}
-      </button>
+      <div class="row align-items-start">
+        <div class="mt-2 mb-2 col-3">
+          <button
+            class="btn btn-success m-1"
+            onClick={() => {
+              setRunning(!running);
+              if (!running) {
+                runningRef.current = true;
+                runSimulation();
+              }
+            }}
+          >
+            {running ? 'Stop' : 'Start'}
+          </button>
+          <button
+            class="btn btn-outline-danger m-1"
+            onClick={() => {
+              setGrid(generateEmptyGrid());
+            }}
+          >
+            Clear
+          </button>
+          <button
+            class="btn btn-outline-warning m-1"
+            onClick={() => {
+              const rows = [];
+              for (let i = 0; i < numRows; i++) {
+                // Randomly generates a state using a value. Random values greater than 0.5 are 1 
+                rows.push(Array.from(Array(numCols), () => Math.random() > .5 ? 1 : 0));
+              }
+              setGrid(rows);
+            }}
+          >
+            Random
+          </button>
 
-      <button
-      class="btn btn-outline-danger m-1"
-      onClick={() => {
-        setGrid(generateEmptyGrid());
-      }}
-      >
-        Clear
-      </button>
-
-      <button
-      class="btn btn-outline-warning m-1"
-      onClick={() => {
-        const rows = []; 
-        for (let i=0; i < numRows; i++){
-          // Randomly generates a state using a value. Random values greater than 0.5 are 1 
-          rows.push(Array.from(Array(numCols),() => Math.random() > 0.5 ? 1 : 0));
-        }
-        setGrid(rows);
-      }}
-      >
-        Random
-      </button>
+        </div>
+        <div class="slidecontainer col-3">
+          <label for="customRange1" class="form-label">Speed</label>
+          <input type="range" step="1" min="1" max="100" class="form-range" id="customRange1" onChange={e => setGameSpeed(e.target.value)}/>
+        </div>
       </div>
+
       <div style={{
         display: "grid",
         gridTemplateColumns: `repeat(${numCols}, 20px)`
